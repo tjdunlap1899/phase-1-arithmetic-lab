41a8bf5e6823e95cc1b9f469ea29ecae1f69c09f
Trevor Dunlap <tjdunlap1899@gmail.com>
2022-12-15 17:23:52 -0500

    commit tree ded3f66f44d8143088043d1d90c872b3ef21cfcb

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31
+let num2 = 2
+let num3 = 16
+let num4 = 6
 
-// Write your code here
+let multiply = (num1 * num2)
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = (num3 % num4);
+
+let max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
